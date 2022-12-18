1853457e43e6b45b1c3808a9968add5a7a7ca19c
gladishd <gladish.dean@gmail.com>
2022-12-18 02:45:38 -0500

    comments out /build from .gitignore and runs npm run build

---
+++ build/precache-manifest.a78fe2cc8929e3871cb9b84dafd885b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17df33cd19ce0d9381525942a3e5533a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93e6e9df9be90a3bebff",
+    "url": "/static/css/main.b07b867b.chunk.css"
+  },
+  {
+    "revision": "718a3ad032c86a9f4148",
+    "url": "/static/js/2.2ece2df3.chunk.js"
+  },
+  {
+    "revision": "296362f3435e3c4e1832bddadf6a9199",
+    "url": "/static/js/2.2ece2df3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93e6e9df9be90a3bebff",
+    "url": "/static/js/main.95499c95.chunk.js"
+  },
+  {
+    "revision": "194e2ee0f4cc59fed6f5",
+    "url": "/static/js/runtime-main.9405ae73.js"
+  }
+]);
